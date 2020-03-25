142be47f2118784956ad30192d937339160255ae
Jeff Gunderson <jgunderson819@gmail.com>
2020-03-25 15:58:30 -0500

    final push

--- db/burgers_db.sql
+++ db/burgers_db.sql
@@ -13,4 +13,9 @@ PRIMARY KEY (id)
 
 )
 
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('California Burger');
+INSERT INTO burgers (burger_name) VALUES ('Vegan Burger');
+INSERT INTO burgers (burger_name) VALUES ('Everything Burger');
 
